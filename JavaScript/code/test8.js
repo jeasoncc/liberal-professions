56c4ed4f3526fdd9fcc532957d3a6247a5ad301e
jeasonchen <chenzhang7618@163.com>
2022-04-14 17:40:08 +0800

    feat: 增加市场书籍文档，JavaScript 文档

--- JavaScript/code/test8.js
+++ JavaScript/code/test8.js
@@ -1,24 +1,44 @@
-var obj = {
-    a: 2
-  };
+// var obj = {
+//     a: 2
+//   };
   
-  obj.a = 3;
-  obj.a // 3
+//   obj.a = 3;
+//   obj.a // 3
   
-  Object.defineProperty(obj, 'a', {
-    value: 4,
-    writable: true,
-    configurable: false, //  不可配置
-    enmerable: true,
-  });
+//   Object.defineProperty(obj, 'a', {
+//     value: 4,
+//     writable: true,
+//     configurable: false, //  不可配置
+//     enmerable: true,
+//   });
   
-  obj.a // 4
-  obj.a = 5
-  obj // 5
+//   obj.a // 4
+//   obj.a = 5
+//   obj // 5
   
-  Object.defineProperty(obj, 'a', {
-    value: 6,
-    writable: true,
-    configurable: true,
-    enmerable: true,
-  }); // TypeError: Cannot redefine property: a
+//   Object.defineProperty(obj, 'a', {
+//     value: 6,
+//     writable: true,
+//     configurable: true,
+//     enmerable: true,
+//   }); // TypeError: Cannot redefine property: a
+
+var randoms = {
+  [Symbol.iterator]: function (){
+    return {
+      next: function() {
+        return {
+          value: Math.random()
+        }
+      }
+    }
+  }
+}
+
+var random_pool = []
+for(var n of randoms) {
+  random_pool.push(n );
+  // 防止无限运行
+  if(random_pool.length === 10000) break
+}
+console.log(random_pool)
